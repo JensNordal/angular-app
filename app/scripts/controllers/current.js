df9c2e1aedee88deffe92db6a90164171f4f7599
JensNordal <jensnordal@gmail.com>
2016-05-22 21:18:39 -0700

    Add animation and css

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -8,37 +8,42 @@
  * Controller of the angularAppApp
  */
 angular.module('angularAppApp')
-  .controller('CurrentCtrl', function ($scope, $routeParams, current, $localStorage) {
-    $scope.cityID = $routeParams.cityID;
+    .controller('CurrentCtrl', function($scope, $routeParams, current, $localStorage) {
+        $scope.cityID = $routeParams.cityID;
 
-    $scope.currentWeather = current.query({
-        cityID: $routeParams.cityID
-    });
+        $scope.currentWeather = current.query({
+            cityID: $routeParams.cityID
+        });
 
-    $scope.saveCity = function(city){
-        var cityData = {
-            'name': city.name,
-            'id': city.id
-        };
-        if (!$localStorage.savedCities){
-            $localStorage.savedCities = [cityData];
-        } else {
-            // We have already saved some cities. 
-            // Check to make sure we haven't already saved the current city.
-            var save = true; // Initialize the save decision variable.
-            // Use this loop to check if we've already saved the city.
-            for (var i=0; i < $localStorage.savedCities.length; i++){
-                if ($localStorage.savedCities[i].id === cityData.id) {
-                    // This is a duplicate, so don't save (variable set to false).
-                    save = false;
+        $scope.saveCity = function(city) {
+            var cityData = {
+                'name': city.name,
+                'id': city.id
+            };
+            if (!$localStorage.savedCities) {
+                $localStorage.savedCities = [cityData];
+            } else {
+                // Check to make sure we haven't already saved the city.
+                var save = true;
+                for (var i = 0; i < $localStorage.savedCities.length; i++) {
+                    if ($localStorage.savedCities[i].id === cityData.id) {
+                        // this is a duplicate, so don't save
+                        save = false;
+                    }
+                }
+                if (save === true) {
+                    $localStorage.savedCities.push(cityData);
+                    // Add object to trigger messages
+                    $scope.citySaved = {
+                        'success': true
+                    };
+                } else {
+                    console.log('city already saved');
+                    // Add object to trigger messages
+                    $scope.citySaved = {
+                        'duplicate': true
+                    };
                 }
             }
-            if (save===true){
-                $localStorage.savedCities.push(cityData);
-                console.log('city saved');
-            } else {
-                console.log('city already saved');
-            }
-        }
-    };
-  });
+        };
+    });
